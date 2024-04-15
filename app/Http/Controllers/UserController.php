6cf96c9319b34db4bd6ca9835d4d5b05571d6e28
Lopeinapauline <165674178+Lopeinapauline@users.noreply.github.com>
2024-04-15 19:11:16 +0700

    [ADD] feature get profile current user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Http\Requests\UserRegisterRequest;
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\support\Str;
@@ -16,9 +19,12 @@ class UserController extends Controller
     public function register(UserRegisterRequest $request): JsonResponse
     {
         $user = User::create($request->validated());
+        
         return (new UserResource($user))->response()->setStatusCode(201);
     }
+    
     public function login(UserLoginRequest $request): UserResource
+    
     {
         $data = $request->validated();
         $user = User::where('email', $data['email'])->first();

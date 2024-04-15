09201d7c1753103cd64651d92bb34337b5e99845
Lopeinapauline <165674178+Lopeinapauline@users.noreply.github.com>
2024-04-15 16:59:47 +0700

    [ADD] feature get profile current userdate UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,4 +1,21 @@
-<?php
+use Illuminate\Http\JsonResponse;
+use Illuminate\support\Str;
+use Illuminate\support\Facades\Hash;
+use Illuminate\support\Facades\Auth;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserController extends Controller
+@@ -38,4 +39,10 @@ public function login(UserLoginRequest $request): UserResource
+        $user->save();
+        return new UserResource($user);
+    }
+
+    public function get(Request $request): UserResource
+    {
+        $user = Auth::user();
+        return new UserResource($user);
+    }
+}<?php
 
 namespace App\Http\Controllers;
 
